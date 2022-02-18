c3ff7f235ad850349d4e93b7fba8776b8598fb9d
Laura Ruuska <laura.ruuska@helsinki.fi>
2022-02-18 10:49:59 +0200

    generate with first letter created

--- NameGenerator-Tira/src/main/java/namegenerator/Generator.java
+++ NameGenerator-Tira/src/main/java/namegenerator/Generator.java
@@ -34,7 +34,7 @@ public class Generator {
         if (k < 0 || n <= 0 || n > 25) {
             return "Bad parameters";
         }
-        int[] history = new int[26];
+        int[] history = new int[n + 1];
         TrieNode node = this.trie.getRoot();
         int knownLetters = 0;
         int firstInBranch = 0;
@@ -61,4 +61,45 @@ public class Generator {
         }
         return constructName(history);
     }
+    
+    /** Generates name from trie using k-degree Markov chain and given first 
+     * letter.
+     * @param k degree of Markov chain, must be positive
+     * @param n max length of name, between 1 and 25
+     * @return generated name as String
+     */
+    public String generateNameWithFirstLetter(int k, int n, String letter) {
+        if (k < 0 || n <= 0 || n > 25) {
+            return "Bad parameters";
+        }
+        int[] history = new int[n + 1];
+        int firstLetter = letter.charAt(0);
+        TrieNode node = this.trie.getRoot();
+        int knownLetters = 1;
+        history[0] = firstLetter;
+        int firstInBranch = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (i - k > 0) {
+                firstInBranch = i - k;
+            } 
+            for (int j = firstInBranch; j < knownLetters; j++) { //crawl to right place
+                node = node.getChildren()[history[j]];
+            }
+            
+            int newIndex = this.trie.getMostPopularIndex(node);
+            if (newIndex <= 0) {
+                newIndex = trie.getNodeWithChildren(node);
+                if (newIndex <= 0) {
+                    break; //node has no children, give up
+                }
+            }
+            history[knownLetters] = newIndex;
+            knownLetters++;
+            node = this.trie.getRoot();
+        }
+        return constructName(history);
+    }
+ 
+
 }
